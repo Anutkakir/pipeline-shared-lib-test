bfc6c3674759d084bd89c93f580ce707d5ce87a4
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 16:15:23 +0300

    test

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,22 +1,16 @@
-//def info(message) {
-//    echo "INFO: ${message}"
-//}
-//
-//def warning(message) {
-//    echo "WARNING: ${message}"
-//}
-
-
-def warning(message) {
+def printMessage(color, type, message) {
     ansiColor('xterm') {
-        println "\033[90m * * * * [WARNING] - ${message} \033[0m "
+        println("\033[${color} * * * * [${type}] - ${message} \033[0m ")
     }
 }
 
+
+def warning(message) {
+    printMessage("1;32m", "Warning", message)
+}
+
 def error(message, exit = true) {
-    ansiColor('xterm') {
-        println "\033[1;31m * * * * [Error] - ${message} \033[0m "
-    }
+    printMessage("1;31m", "Error", message)
 
     if ( exit ) {
         sh 'exit 1'
@@ -24,27 +18,18 @@ def error(message, exit = true) {
 }
 
 def info(message) {
-    ansiColor('xterm') {
-        println "\033[1;34m * * * * [Info] - ${message} \033[0m "
-    }
+    //ansiColor('xterm') {
+    //    println "\033[1;34m * * * * [Info] - ${message} \033[0m "
+    //}
+    printMessage("1;34m", "Info", message)
 }
 
 
 def debug(message) {
-    ansiColor('xterm') {
-        println "\033[1;30m * * * * [DEBUG] - ${message} \033[0m "
-    }
+    printMessage("1;30m", "Debug", message)
 }
 
 
-//def call(body) {
-//    new Log(script:this).run()
-//    script:this.echo(new Log(1, 3))
-//
-//    echo "Deployed"
-//
-//    return this
-//}
 
 
 
