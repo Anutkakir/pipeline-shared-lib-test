935ed8d5712085754da5ef920a2df398ef13fd7a
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 17:50:55 +0300

    test

--- vars/skype.groovy
+++ vars/skype.groovy
@@ -5,3 +5,5 @@ def sendMessage(message, channel, skypeSecret, skypeURL){
     def skypeResponse = httpRequest(skypeURL, skypeData, '', 'POST', headers=[[name: '', value: '']])
     log.debug("skypeResponse - ${skypeResponse}")
 }
+
+sendMessage("test", "ann_test", "env.SKYPE_SECRET", "env.SKYPE_MSG_URL")
