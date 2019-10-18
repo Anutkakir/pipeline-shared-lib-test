fa4c121026f8654bac7367b6743dfd104e606014
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 17:38:28 +0300

    test

--- vars/httpRequest.groovy
+++ vars/httpRequest.groovy
@@ -1,4 +1,4 @@
-def commonHttpRequest(url, data, auth, mode, headers=[[name: '', value: '']]){
+def call(body) {
     response = httpRequest authentication: auth,
             acceptType: 'APPLICATION_JSON_UTF8',
             contentType: 'APPLICATION_JSON_UTF8',
@@ -10,5 +10,22 @@ def commonHttpRequest(url, data, auth, mode, headers=[[name: '', value: '']]){
             url: url,
             quiet: true
 
-    return response
+    //return response
+    return this
 }
+
+
+//def commonHttpRequest(url, data, auth, mode, headers=[[name: '', value: '']]){
+//    response = httpRequest authentication: auth,
+//            acceptType: 'APPLICATION_JSON_UTF8',
+//            contentType: 'APPLICATION_JSON_UTF8',
+//            customHeaders: headers,
+//            httpMode: mode,
+//            ignoreSslErrors: true,
+//            requestBody: data,
+//            validResponseCodes: '100:500',
+//            url: url,
+//            quiet: true
+//
+//    return response
+//}
