9e02fac8aa8a04d693f5b0ddba5187d5f57e82ac
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 17:23:04 +0300

    test

--- vars/sendSkypeMessage.groovy
+++ vars/sendSkypeMessage.groovy
@@ -1,5 +1,5 @@
 def sendSkypeMessage(message, channel, skypeSecret, skypeURL){
-    log.debug(p_message)
+    log.debug(message)
     def skypeData = '{"channel":"' + channel + '","secret":"' + skypeSecret + '","type":"simple","text":"' + message + '"}'
     log.debug("skypeData - ${skypeData}")
     //def skypeResponse = CommonHttpRequest.CommonHttpRequest(skypeURL, skypeData, '', 'POST')
