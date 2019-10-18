3932f3df39827b6119a38ce76aab8df742c879c0
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 17:53:40 +0300

    test

--- vars/skype.groovy
+++ vars/skype.groovy
@@ -1,9 +1,25 @@
+def commonHttpRequest(url, data, auth, mode, headers=[[name: '', value: '']]){
+    response = httpRequest authentication: auth,
+            acceptType: 'APPLICATION_JSON_UTF8',
+            contentType: 'APPLICATION_JSON_UTF8',
+            customHeaders: headers,
+            httpMode: mode,
+            ignoreSslErrors: true,
+            requestBody: data,
+            validResponseCodes: '100:500',
+            url: url,
+            quiet: true
+
+    return response
+}
+
+
 def sendMessage(message, channel, skypeSecret, skypeURL){
     log.debug(message)
     def skypeData = '{"channel":"' + channel + '","secret":"' + skypeSecret + '","type":"simple","text":"' + message + '"}'
     log.debug("skypeData - ${skypeData}")
-    def skypeResponse = httpRequest(skypeURL, skypeData, '', 'POST', headers=[[name: '', value: '']])
+    def skypeResponse = commonHttpRequest(skypeURL, skypeData, '', 'POST', headers=[[name: '', value: '']])
     log.debug("skypeResponse - ${skypeResponse}")
 }
 
-sendMessage("test", "ann_test", "env.SKYPE_SECRET", "env.SKYPE_MSG_URL")
+//sendMessage("test", "ann_test", "env.SKYPE_SECRET", "env.SKYPE_MSG_URL")
