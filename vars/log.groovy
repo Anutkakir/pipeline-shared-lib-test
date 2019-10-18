5b06b8708d0e68d947740c01a394c461d7d416be
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 16:02:39 +0300

    test

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,50 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//    echo "WARNING: ${message}"
+//}
+
 
 def warning(message) {
-    echo "WARNING: ${message}"
+    ansiColor('xterm') {
+        println "\\033[90m * * * * [WARNING] - ${message} \033[0m "
+    }
 }
+
+def error(message, exit = true) {
+    ansiColor('xterm') {
+        println "\033[1;31m * * * * [Error] - ${message} \033[0m "
+    }
+
+    if ( exit ) {
+        sh 'exit 1'
+    }
+}
+
+def info(message) {
+    ansiColor('xterm') {
+        println "\033[1;34m * * * * [Info] - ${message} \033[0m "
+    }
+}
+
+
+def debug(message) {
+    ansiColor('xterm') {
+        println "\033[1;30m * * * * [DEBUG] - ${message} \033[0m "
+    }
+}
+
+
+//def call(body) {
+//    new Log(script:this).run()
+//    script:this.echo(new Log(1, 3))
+//
+//    echo "Deployed"
+//
+//    return this
+//}
+
+
+
