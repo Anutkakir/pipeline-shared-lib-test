7c10c4441521e45249577bfd292d8411edc5f794
Anna_Stankova <Anna_Stankova@epam.com>
2019-10-18 17:36:37 +0300

    test

--- vars/skype.groovy
+++ vars/skype.groovy
@@ -2,6 +2,6 @@ def sendMessage(message, channel, skypeSecret, skypeURL){
     log.debug(message)
     def skypeData = '{"channel":"' + channel + '","secret":"' + skypeSecret + '","type":"simple","text":"' + message + '"}'
     log.debug("skypeData - ${skypeData}")
-    def skypeResponse = httpRequest.CommonHttpRequest(skypeURL, skypeData, '', 'POST')
+    def skypeResponse = httpRequest.commonHttpRequest(skypeURL, skypeData, '', 'POST', headers=[[name: '', value: '']])
     log.debug("skypeResponse - ${skypeResponse}")
 }
